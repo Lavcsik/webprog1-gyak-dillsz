66213abe0013a0fb063de1d039a509521c582b7c
StromanDILLSZ2 <dominik.laucsik13@gmail.com>
2026-05-08 23:28:20 +0200

    Adatbázis adatok átírása online működésre

--- a/templates/pages/uzenetfal.tpl.php
+++ a/templates/pages/uzenetfal.tpl.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "gyakorlat7";
+    $servername = "mysql.omega:3306";
+    $username = "pizza";
+    $password = "pizza20260502";
+    $dbname = "pizza";
     try {
         $connect = new PDO("mysql:host=" . $servername . "; dbname=" . $dbname, $username, $password);
         $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
